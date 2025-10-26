d72d1ca928557e62707ce3773c36e46b50ae0140
varun bansal <varunbansal@varuns-MacBook-Pro.local>
2025-10-26 09:52:37 +0530

    added variables

---
+++ variable.js
@@ -0,0 +1,16 @@
+/*
+
+const -> constanat value cannot be reassigned 
+let -> variable value 
+Semicolons are optional in javascript 
+
+
+*/
+const value =12345
+
+let variable='xyz'
+
+variable='hjdfjsdhmfj'
+
+console.log(value)
+console.log(variable)
